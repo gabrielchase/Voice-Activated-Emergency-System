01cfdf93c19ddd70c7ea9333f1ee95b0a0ae483e
gabrielchase <gchase.patron@gmail.com>
2019-05-05 15:54:14 +0800

    Let client tag emergency as seen

--- server/models/Emergency.js
+++ server/models/Emergency.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 let EmergencySchema = new mongoose.Schema({
     latitude: { type: Number, required: true },
     longitude: { type: Number, required: true },
-    
+    seen: [mongoose.Mixed],
+
     created_on: { type: Date, default: Date.now }
 })
 
@@ -11,6 +12,28 @@ EmergencySchema.statics.create = async function ({ latitude, longitude }) {
     const emergency = new this({ latitude, longitude })
     await emergency.save()
     return emergency
+}
+
+EmergencySchema.statics.seen = async function (emergency_id, user) {
+    const emergency = await this.findById(emergency_id)
+    const user_id = user._id
+
+    for (const obj of emergency.seen) {
+        if (obj.user_id === user_id) {
+            return obj 
+        }
+    }
+
+    let seen_data = {
+        user_id, 
+        date: new Date()
+    }
+
+    emergency.seen.push(seen_data)
+
+    await emergency.save()
+
+    return seen_data
 }
 
 module.exports = mongoose.model('Emergency', EmergencySchema)
--- server/routes/emergency.js
+++ server/routes/emergency.js
@@ -1,6 +1,7 @@
 const Emergency = require('../models/Emergency')
 
 const { success, fail } = require('../lib/json_wrappers')
+const { checkJWTUser } = require('../middleware/jwt')
 
 module.exports = (app) => {
     app.post('/api/emergency', async (req, res) => {
@@ -9,6 +10,16 @@ module.exports = (app) => {
             success(res, new_emergency)
         } catch (err) {
             fail(res, err)
+        }
+    })
+
+    app.put('/api/emergency/:emergency_id/seen', checkJWTUser, async (req, res) => {
+        const { emergency_id } = req.params
+        try {
+            const seen_emergency = await Emergency.seen(emergency_id, req.user)
+            success(res, seen_emergency)
+        } catch (err) {
+            fail(res, err)            
         }
     })
 }
