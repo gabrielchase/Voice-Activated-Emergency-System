f78026780c19db7dfe5721143e194229ce4510a9
gabrielchase <gchase.patron@gmail.com>
2019-05-05 19:10:07 +0800

    Dashboard component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,48 @@
 import React from 'react'
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react'
+import { ApiConsumer } from '../providers/ApiProvider'
+
+import { GOOGLE_API_KEY } from '../config/config'
 
 class Dashboard extends React.Component {
+    state = {
+        center: {
+            lat: 14.6538,
+            lng: 121.0685
+        },
+        zoom: 16
+    }
+
     render() {
+        const { current_user } = this.props
+        console.log('curent_user: ', current_user)
         return (
             <div>
-                Dashboard Page
+                <p>Dashboard Page</p>
+                <p>Good day {current_user.name}</p>
+                <Map 
+                    className={'google-map'}
+                    google={this.props.google} 
+                    zoom={this.state.zoom} 
+                    initialCenter={this.state.center}
+                />
             </div>
         )
     }
 }
 
-export default Dashboard 
+const ConnectedDashboard = props => (
+    <ApiConsumer >
+        {({ current_user }) => (
+            <Dashboard 
+                {...props}
+                current_user={current_user}
+            />
+        )}
+    </ApiConsumer>
+)
+
+export default GoogleApiWrapper({
+    apiKey: (GOOGLE_API_KEY)
+})(ConnectedDashboard)
+
