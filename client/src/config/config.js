2f27329aa0af31a08c410f27047bbdff1671b853
gabrielchase <gchase.patron@gmail.com>
2019-05-07 01:04:03 +0800

    Add ROOT URL to config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -5,6 +5,7 @@ console.log('Config file: ', config_file)
 let config = {}
 
 try {
+    config.ROOT_URL = process.env.ROOT_URL || config_file.ROOT_URL 
     config.API_URL = process.env.API_URL || config_file.API_URL 
     config.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || config_file.GOOGLE_API_KEY 
 } catch (err) {
