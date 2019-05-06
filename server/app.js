1f14f566b2a5cc3ce54220e7183d9a7553b5456e
gabrielchase <gchase.patron@gmail.com>
2019-05-07 01:10:54 +0800

    Add io to app

--- server/app.js
+++ server/app.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const config = require('./config/config')
-
-console.log('CONFIG: ', config)
+const app = new express()
 
 mongoose.connect(config.DB_URL, { useNewUrlParser: true })
 mongoose.Promise = global.Promise
@@ -12,8 +11,6 @@ mongoose.set('debug', true)
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error'))
-
-const app = new express()
 
 app.use(bodyParser.json({ limit: '192mb' }))
 app.use(bodyParser.urlencoded({ extended: true })) // to support URL-encoded bodies
@@ -23,11 +20,14 @@ app.get('/api/sanity', (_, res) => {
     res.json({ sane: true })
 })
 
-require('./routes/auth')(app)
-require('./routes/emergency')(app)
-
-app.listen(config.PORT, () => {
+const server = app.listen(config.PORT, () => {
     console.log(`Server running on ${config.PORT}`)
 })
+const io = require('socket.io')(server)
+
+require('./routes/auth')(app)
+require('./routes/emergency')(app, io)
+
+require('./socket_api')(io)
 
 module.exports = { app }
